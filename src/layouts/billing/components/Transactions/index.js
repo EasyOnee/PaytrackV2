a6ba0305f16ade3212c1eb38d4f193a9f4d1f61d
Luis Peraza <warriorpropvp@hotmail.com>
2024-03-05 09:35:45 -0700

    Cambios generales

--- src/layouts/billing/components/Transactions/index.js
+++ src/layouts/billing/components/Transactions/index.js
@@ -61,14 +61,14 @@ function Transactions() {
           <Transaction
             color="error"
             icon="expand_more"
-            name="Netflix"
+            name="Materia reprobada"
             description="27 March 2020, at 12:30 PM"
             value="- $ 2,500"
           />
           <Transaction
             color="success"
             icon="expand_less"
-            name="Apple"
+            name="Cuatrimestre 8"
             description="27 March 2020, at 04:30 AM"
             value="+ $ 2,000"
           />
@@ -89,28 +89,28 @@ function Transactions() {
           <Transaction
             color="success"
             icon="expand_less"
-            name="Stripe"
+            name="Cuatrimestre 1"
             description="26 March 2020, at 13:45 PM"
             value="+ $ 750"
           />
           <Transaction
             color="success"
             icon="expand_less"
-            name="HubSpot"
+            name="Cuatrimestre 2"
             description="26 March 2020, at 12:30 PM"
             value="+ $ 1,000"
           />
           <Transaction
             color="success"
             icon="expand_less"
-            name="Creative Tim"
+            name="Cuatrimestre 3"
             description="26 March 2020, at 08:30 AM"
             value="+ $ 2,500"
           />
           <Transaction
             color="dark"
             icon="priority_high"
-            name="Webflow"
+            name="Cuatrimestre 4"
             description="26 March 2020, at 05:00 AM"
             value="Pending"
           />
--- src/layouts/dashboard/components/OrdersOverview/index.js
+++ src/layouts/dashboard/components/OrdersOverview/index.js
@@ -48,31 +48,31 @@ function OrdersOverview() {
         <TimelineItem
           color="success"
           icon="notifications"
-          title="$2400, Design changes"
+          title="cargos ID #4395133"
           dateTime="22 DEC 7:20 PM"
         />
         <TimelineItem
           color="error"
           icon="inventory_2"
-          title="New order #1832412"
+          title="cargos ID #4395133"
           dateTime="21 DEC 11 PM"
         />
         <TimelineItem
           color="info"
           icon="shopping_cart"
-          title="Server payments for April"
+          title="cargos ID #4395133"
           dateTime="21 DEC 9:34 PM"
         />
         <TimelineItem
           color="warning"
           icon="payment"
-          title="New card added for order #4395133"
+          title="cargos ID #4395133"
           dateTime="20 DEC 2:20 AM"
         />
         <TimelineItem
           color="primary"
           icon="vpn_key"
-          title="New card added for order #4395133"
+          title="cargos ID #4395133"
           dateTime="18 DEC 4:54 AM"
           lastItem
         />
